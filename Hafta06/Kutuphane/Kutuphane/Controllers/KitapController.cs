2c0e223edbf2c37455c0784da3c2285e85d4212c
ibrahimayaz <ayazibrahim13@gmail.com>
2023-11-09 15:49:47 +0300

    Hafta06

---
+++ Hafta06/Kutuphane/Kutuphane/Controllers/KitapController.cs
@@ -0,0 +1,27 @@
+﻿using Kutuphane.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kutuphane.Controllers
+{
+    public class KitapController:Controller
+    {
+        public static List<Kitap> kitaplar = new();
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ekle(Kitap k)
+        {
+            kitaplar.Add(k);
+            return View("Liste", kitaplar);
+        }
+
+        public IActionResult Liste()
+        {
+            return View(kitaplar);
+        }
+
+    }
+}
---
+++ Hafta06/Kutuphane/Kutuphane/Models/Kitap.cs
@@ -0,0 +1,18 @@
+﻿namespace Kutuphane.Models
+{
+    public class Kitap
+    {
+        public String ISSN { get; set; }
+        public String Ad { get; set; } = String.Empty;
+        public String Yazar{ get; set; } = String.Empty;
+        public String ResimYolu { get; set; } = String.Empty;
+        public double Fiyat { get; set; }
+        public DateTime BasimTarihi { get; set; }
+        public int SayfaSayisi { get; set; }
+
+        public Kitap()
+        {
+            BasimTarihi=DateTime.Now;
+        }
+    }
+}
